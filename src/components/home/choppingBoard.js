5f068df1b411426054c7f2f1e0992b39415aa6f1
Prawsang <prawcha.p@gmail.com>
2019-07-14 02:42:00 +0700

    init scrolling effects

---
+++ src/components/home/choppingBoard.js
@@ -0,0 +1,71 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Image = ({ name, className, style }) => (
+    <StaticQuery
+        query={graphql`
+        query {
+          choppingBoard: file(relativePath: { eq: "viet_noodle/chopping_board_dark.png" }) {
+            ...fullWidthImage
+          }
+        }
+      `}
+        render={data => (
+            <Img
+                fluid={data[name].childImageSharp.fluid}
+                className={className}
+                style={style}
+            />
+        )}
+    />
+)
+
+class ChoppingBoard extends React.Component {
+    state = {
+        scroll: 0,
+        top: 0,
+        pageHeight: 0
+    }
+    componentDidMount() {
+        this.setState({
+            top: ReactDOM.findDOMNode(this).getBoundingClientRect().top,
+            pageHeight: window.screen.availHeight
+        });
+        this.listenToScroll = this.listenToScroll.bind(this);
+        window.addEventListener('scroll', this.listenToScroll);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.listenToScroll)
+    }
+
+    listenToScroll() {
+        const pagePosition = window.pageYOffset;
+        const scroll = pagePosition - this.state.top;
+
+        if (scroll > 0) {
+            this.setState({ scroll })
+        } else {
+            this.setState({ scroll: 0 })
+        }
+        this.changeLights();
+    }
+    changeLights() {
+        const { scroll, pageHeight } = this.state;
+        const phase = Math.floor(((scroll + pageHeight) / pageHeight));
+        return "phase-" + (phase <= 4 ? phase : 4)
+    }
+    render() {
+        return (
+            <div className={`chopping-board-image-wrapper ${this.changeLights()}`}>
+                {/* <div className="chopping-board-image-wrapper"> */}
+                <Image name="choppingBoard" className="chopping-board-image" />
+                <div className="light-circle light-1" />
+                <div className="light-circle light-2" />
+            </div>
+        )
+    }
+}
+
+export default ChoppingBoard;
--- src/components/home/ingr.js
+++ src/components/home/ingr.js
@@ -1,57 +1,38 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import ChoppingBoard from "./choppingBoard"
 
-const Image = ({ name, className, style }) => (
-    <StaticQuery
-        query={graphql`
-        query {
-          choppingBoard: file(relativePath: { eq: "viet_noodle/chopping_board_dark.png" }) {
-            ...fixed500
-          }
-        }
-      `}
-        render={data => (
-            <Img
-                fixed={data[name].childImageSharp.fixed}
-                className={className}
-                style={style}
-            />
-        )}
-    />
-)
 
-const Ingredients = () => {
-    return (
-        <div className="ingr-viet-noodle">
-            <div className="container content">
-                <div className="row is-ai-stretch">
-                    <div className="col-6 col-has-padding chopping-board">
-                        <div className="chopping-board-image-wrapper">
-                            <Image name="choppingBoard" className="chopping-board-image" />
+class Ingredients extends React.Component {
+    render() {
+        return (
+            <div className="ingr-viet-noodle">
+                <div className="container content">
+                    <div className="row is-ai-stretch">
+                        <div className="col-6 col-has-padding chopping-board">
+                            <ChoppingBoard />  
+                        </div>
+                        <div className="col-6 col-has-padding chopping-board">
+                            <div className="ingr-item texture-1 masked">
+                                <h1 style={{ color: 'white' }}>ผงปรุงน้ำซุป</h1>
+                                <h3 className="p masked-item">ผงปรุงน้ำซุปสกัดจากน้ำต้มกระดูก หมู รสน้ำซุปหวานน้ำต้มกระดูกหมู  กลมกล่อม หอมและเผ็ดร้อนด้วย พริกไทยดำ</h3>
+                            </div>
+                            <div className="ingr-item texture-1 masked">
+                                <h1 style={{ color: 'white' }}>เส้นสด</h1>
+                                <h3 className="p masked-item">เส้นสดจากอุบลแท้ เหนียวนุ่ม</h3>
+                            </div>
+                            <div className="ingr-item">
+                                <h1>หอมเจียวและพริกผัด</h1>
+                            </div>
+                            <div className="ingr-item">
+                                <h3>เพื่อเพิ่มความอร่อยและคุณค่าทางโภชนาการ</h3>
+                                <h6 className="p">สามารถเติมผักและเนื้อสัตว์ต่าง ๆ เช่น ผักชีใบเลื่อย และหมูยอ</h6>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-6 col-has-padding chopping-board">
-                        <div className="ingr-item texture-1 masked">
-                            <h1 style={{ color: 'white' }}>ผงปรุงน้ำซุป</h1>
-                            <h3 className="p masked-item">ผงปรุงน้ำซุปสกัดจากน้ำต้มกระดูก หมู รสน้ำซุปหวานน้ำต้มกระดูกหมู  กลมกล่อม หอมและเผ็ดร้อนด้วย พริกไทยดำ</h3>
-                        </div>
-                        <div className="ingr-item texture-1 masked">
-                            <h1 style={{ color: 'white' }}>เส้นสด</h1>
-                            <h3 className="p masked-item">เส้นสดจากอุบลแท้ เหนียวนุ่ม</h3>
-                        </div>
-                        <div className="ingr-item">
-                            <h1>หอมเจียวและพริกผัด</h1>
-                        </div>
-                        <div className="ingr-item">
-                            <h3>เพื่อเพิ่มความอร่อยและคุณค่าทางโภชนาการ</h3>
-                            <h6 className="p">สามารถเติมผักและเนื้อสัตว์ต่าง ๆ เช่น ผักชีใบเลื่อย และหมูยอ</h6>
-                        </div>
-                     </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default Ingredients;
