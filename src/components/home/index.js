9f712bf162b094692bbe170535b9d785596c4ac0
Prawsang <prawcha.p@gmail.com>
2019-07-14 00:58:38 +0700

    init ingredients section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,32 +1,14 @@
 import React from "react"
+
 import Hero from "./hero"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-
-
-const Image = ({ name, className, style }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        choppingBoard: file(relativePath: { eq: "viet_noodle/chopping_board_dark.png" }) {
-          ...fixed250
-        }
-      }
-    `}
-    render={data => (
-      <Img
-        fixed={data[name].childImageSharp.fixed}
-        className={className}
-        style={style}
-      />
-    )}
-  />
-)
+import Ingredients from "./ingr"
 
 const HomePage = () => (
   <React.Fragment>
     <Hero />
-    <Image name="choppingBoard" />
+    <main className="container full-width dark">
+      <Ingredients />
+    </main>
   </React.Fragment>
 )
 
