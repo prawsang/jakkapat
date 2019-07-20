25ca9db9a8f48ea009505bdb1793eeb2d58ad8a8
Prawsang <prawcha.p@gmail.com>
2019-07-20 21:21:32 +0700

    add more info to instructions

--- src/components/home/ingr.js
+++ src/components/home/ingr.js
@@ -5,33 +5,35 @@ import ChoppingBoard from "./choppingBoard"
 class Ingredients extends React.Component {
     render() {
         return (
-            <div className="ingr-viet-noodle section">
-                <div className="container content">
-                    <h1 className="center" style={{ marginBottom: '128px' }}>ในซองมีอะไรบ้าง ?</h1>
-                    <div className="row is-ai-stretch is-jc-space-between">
-                        <div className="col-6 col-has-padding chopping-board">
-                            <ChoppingBoard />
-                        </div>
-                        <div className="col-6 col-has-padding">
-                            <div className="ingr-item texture-1 masked">
-                                <h1 style={{ color: 'white' }}>ผงปรุงน้ำซุป</h1>
-                                <h3 className="p masked-item">ผงปรุงน้ำซุปสกัดจากน้ำต้มกระดูก หมู รสน้ำซุปหวานน้ำต้มกระดูกหมู  กลมกล่อม หอมและเผ็ดร้อนด้วย พริกไทยดำ</h3>
+            <section className="section">
+                <div className="ingr-viet-noodle section">
+                    <div className="container content">
+                        <h1 className="center" style={{ marginBottom: '128px' }}>ในซองมีอะไรบ้าง ?</h1>
+                        <div className="row is-ai-stretch is-jc-space-between">
+                            <div className="col-6 col-has-padding chopping-board">
+                                <ChoppingBoard />
                             </div>
-                            <div className="ingr-item texture-1 masked">
-                                <h1 style={{ color: 'white' }}>เส้นสด</h1>
-                                <h3 className="p masked-item">เส้นสดจากอุบลแท้ เหนียวนุ่ม</h3>
-                            </div>
-                            <div className="ingr-item">
-                                <h1>หอมเจียวและพริกผัด</h1>
-                            </div>
-                            <div className="ingr-item">
-                                <h3>เพื่อเพิ่มความอร่อยและคุณค่าทางโภชนาการ</h3>
-                                <h6 className="p">สามารถเติมผักและเนื้อสัตว์ต่าง ๆ เช่น ผักชีใบเลื่อย และหมูยอ</h6>
+                            <div className="col-6 col-has-padding">
+                                <div className="ingr-item texture-1 masked">
+                                    <h1 style={{ color: 'white' }}>ผงปรุงน้ำซุป</h1>
+                                    <h3 className="p masked-item">ผงปรุงน้ำซุปสกัดจากน้ำต้มกระดูก หมู รสน้ำซุปหวานน้ำต้มกระดูกหมู  กลมกล่อม หอมและเผ็ดร้อนด้วย พริกไทยดำ</h3>
+                                </div>
+                                <div className="ingr-item texture-1 masked">
+                                    <h1 style={{ color: 'white' }}>เส้นสด</h1>
+                                    <h3 className="p masked-item">เส้นสดจากอุบลแท้ เหนียวนุ่ม</h3>
+                                </div>
+                                <div className="ingr-item">
+                                    <h1>หอมเจียวและพริกผัด</h1>
+                                </div>
+                                <div className="ingr-item">
+                                    <h3>เพื่อเพิ่มความอร่อยและคุณค่าทางโภชนาการ</h3>
+                                    <h6 className="p">สามารถเติมผักและเนื้อสัตว์ต่าง ๆ เช่น ผักชีใบเลื่อย และหมูยอ</h6>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         )
     }
 }
--- src/components/home/instructions.js
+++ src/components/home/instructions.js
@@ -9,29 +9,47 @@ class Instructions extends React.Component {
         type: 0,
     }
     render() {
+        const { type } = this.state;
         return (
-            <section style={{ paddingTop: 88, paddingBottom: 88 }}>
+            <section className="section">
                 <div className="container narrow white no-overflow has-padding" style={{ paddingTop: 64, paddingBottom: 64 }}>
                     <div className="content center">
                         <h1>ปรุงเองที่บ้านง่าย ๆ</h1>
                         <ul className="tabs">
-                            <li className="tab-item active">ปรุงด้วยการต้ม</li>
-                            <li className="tab-item">ปรุงด้วยไมโครเวฟ</li>
+                            <li className={`tab-item ${type === 0 ? 'active' : ''}`} onClick={() => this.setState({ type: 0 })}>ปรุงด้วยการต้ม</li>
+                            <li className={`tab-item ${type === 1 ? 'active' : ''}`} onClick={() => this.setState({ type: 1 })}>ปรุงด้วยไมโครเวฟ</li>
                         </ul>
-                        <div className="recipe">
-                            <InstItem image={image1} order={1}>
-                                ตวงน้ำ 500-550มล. ใส่ภาชนะ และต้มน้ำให้เดือด
-                            </InstItem>
-                            <InstItem image={image2} order={2}>
-                                ใส่ผงปรุงรสและเส้นสด (หากไม่ชอบให้น้ำซุปข้น ควรล้างเส้นก่อนใส่เล้น) เพื่อเพิ่มคุณค่าทางโภชนาการ สามารถเติมเนื้อสัตว์ เช่น หมูยอ หมูสับ
-                            </InstItem>
-                            <InstItem image={image3} order={3}>
-                                ต้มต่อไปอีก 2-3 นาที
-                            </InstItem>
-                            <InstItem image={image4} order={4} last={true}>
-                                เทใส่ภาชนะ ใส่หอมเจียว พริกผัด และเพื่อเพิ่มความอร่อยสามารถเติมผักต่าง ๆ เช่น ผักชีใบเลื่อย
-                            </InstItem>
-                        </div>
+                        {type === 0 ? (
+                            <div className="recipe">
+                                <InstItem image={image1} order={1}>
+                                    ตวงน้ำ 500-550มล. ใส่ภาชนะ และต้มน้ำให้เดือด
+                                </InstItem>
+                                <InstItem image={image2} order={2}>
+                                    ใส่ผงปรุงรสและเส้นสด (หากไม่ชอบให้น้ำซุปข้น ควรล้างเส้นก่อนใส่เล้น) เพื่อเพิ่มคุณค่าทางโภชนาการ สามารถเติมเนื้อสัตว์ เช่น หมูยอ หมูสับ
+                                </InstItem>
+                                <InstItem image={image3} order={3}>
+                                    ต้มต่อไปอีก 2-3 นาที
+                                </InstItem>
+                                <InstItem image={image4} order={4} last={true}>
+                                    เทใส่ภาชนะ ใส่หอมเจียว พริกผัด และเพื่อเพิ่มความอร่อยสามารถเติมผักต่าง ๆ เช่น ผักชีใบเลื่อย
+                                </InstItem>
+                            </div>
+                        ) : (
+                                <div className="recipe">
+                                    <InstItem order={1}>
+                                        ตวงน้ำ 500-550มล. ใส่ภาชนะ
+                                </InstItem>
+                                    <InstItem order={2}>
+                                        ใส่ผงปรุงรส คนให้เข้ากัน ไม่ให้จับเป็นก่้อน และเพื่อเพิ่มคุณค่าทางโภชนาการ สามารถเติมเนื้อสัตว์ เช่น หมูยอ หมูสับ
+                                </InstItem>
+                                    <InstItem order={3}>
+                                        ใส่เส้น (หากไม่ชอบให้น้ำซุปข้น ควรล้างเส้นก่อนใส่เล้น)
+                                </InstItem>
+                                    <InstItem order={4} last={true}>
+                                        นำเข้าไมโครเวฟเป็นเวลา 5 นาที จากนั้นเติมหอมเจียว พริกผัด และเพื่อเพิ่มคุณค่าทางโภชนาการ สามารถเติมผักอื่นๆ
+                                </InstItem>
+                                </div>
+                            )}
                     </div>
                 </div>
             </section>
@@ -46,13 +64,15 @@ const InstItem = ({ image, order, children, last }) => (
         <div className="col col-2">
             <div className="badge">{order}</div>
         </div>
-        <div className="col col-8">
+        <div className={`col col-${image ? '8' : '10'}`}>
             <p className="larger">
                 {children}
             </p>
         </div>
-        <div className="col col-2">
-            <img src={image} className="image" alt="" style={{ maxWidth: 112 }} />
-        </div>
+        {image && (
+            <div className="col col-2">
+                <img src={image} className="image" alt="" style={{ maxWidth: 112 }} />
+            </div>
+        )}
     </div >
 );
